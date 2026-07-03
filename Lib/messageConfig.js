e9b14f604cae4ac066d054de80002dbcf28fbb19
ranazain3158-lab <ranazain3158@gmail.com>
2026-07-03 08:53:45 +0500

    Add files via upload

---
+++ Lib/messageConfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+    channelInfo: {
+        contextInfo: {
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: '120363427132231647@newsletter',
+                newsletterName: '𝙍𝘼𝙉𝘼 𝙕𝘼𝙄𝙉 MD',
+                serverMessageId: -1
+            }
+        }
+    }
+};
